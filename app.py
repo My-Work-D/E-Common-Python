bced3afc5eae1e4fc5087972a4e2ad6df943cac2
ApsaraWitharana <asachini095@gmail.com>
2024-09-20 17:15:57 +0530

    update- fix error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import bcrypt
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='access')
 
 # Configure MySQL
 app.config['MYSQL_HOST'] = 'localhost'
